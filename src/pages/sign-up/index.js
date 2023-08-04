d17ff7b5f277b0313c1b2f67ea9afa6e14c22432
Valentino Zucchella Paz <vzucchellapaz@gmail.com>
2023-08-04 20:09:02 -0300

    center-sign-up-form

--- src/pages/sign-up/index.js
+++ src/pages/sign-up/index.js
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router';
 export default function SignUp() {
   const router = useRouter();
   return (
-    <section className='w-full h-full flex flex-col items-center gap-6'>
+    <section className='w-full h-[90vh] flex flex-col justify-center items-center gap-6'>
       <div className='w-full md:w-2/3 bg-darkGray flex flex-col gap-3 justify-center items-center px-8 py-3 mt-10 rounded-xl text-white'>
         <AuthForm authParam={headings.signUp} />
       </div>
