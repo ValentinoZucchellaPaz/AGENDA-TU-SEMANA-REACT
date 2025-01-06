8bf20d18ad50c30d09e7f6ce5a577765a87ad5bc
Valentino Zucchella Paz <vzucchellapaz@gmail.com>
2025-01-06 11:10:02 -0300

    auth & context changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 'use client'
+import AuthFallback from "@/components/AuthFallback";
 import TaskContainer from "@/components/TasksContainer";
 import { useFirebaseContentContext } from "@/context/firebaseContentContext";
-import { useRouter } from "next/navigation";
-
 
 export default function Home() {
   const { user } = useFirebaseContentContext()
-  const router = useRouter()
 
   if (user === null) {
-    router.push('/auth')
+    return <AuthFallback />
   }
   return (
     <TaskContainer />
