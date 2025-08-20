e6c10a5a256e9b8192334454b7da5159e249f8db
Valentino Zucchella Paz <vzucchellapaz@gmail.com>
2025-08-20 09:05:13 -0300

    env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,12 +7,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqgAmJX9_5CZdHz_gIM42vFO1gpGwC-Ww",
-  authDomain: 'agenda-tu-semana.firebaseapp.com',
-  projectId: 'agenda-tu-semana',
-  storageBucket: 'agenda-tu-semana.appspot.com',
-  messagingSenderId: '82012225934',
-  appId: '1:82012225934:web:a893140b600771cccae4df',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
